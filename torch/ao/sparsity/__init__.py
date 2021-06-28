f2d470470a510a017b6df6f64f3aa9fa1f135476
Zafar <cc.rafaz@zafar.cc>
2021-06-28 11:20:57 -0700

    [sparsity] WeightNormSparsifier
    Implements the weight norm sparsifier.
    This type of sparsifier computes the norm of the weights, sorts them, and zeroes-out the target fraction of them.
    
    Test Plan:
    
    ```
    python test/test_ao_sparsity.py
    ```
    
    ghstack-source-id: e69dc4fcf9f0a914b6b4b5070b12178f267e63b0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58955

--- torch/ao/sparsity/__init__.py
+++ torch/ao/sparsity/__init__.py
@@ -1,15 +1,14 @@
-from . import sparsifier
-
-# Sparsifier
-from .sparsifier import BaseSparsifier
-
-# Parametrizations
-from .utils import FakeSparsity
-
 # Variables
 from ._mappings import get_sparse_mapping
 from ._mappings import get_dynamic_sparse_quantized_mapping
 from ._mappings import get_static_sparse_quantized_mapping
+
+# Sparsifier
+from .sparsifier.base_sparsifier import BaseSparsifier
+from .sparsifier.weight_norm_sparsifier import WeightNormSparsifier
+
+# Parametrizations
+from .sparsifier.utils import FakeSparsity
 
 # === Experimental ===
 
@@ -18,3 +17,4 @@ from .experimental.pruner.parametrization import PruningParametrization
 
 # Pruner
 from .experimental.pruner.base_pruner import BasePruner
+
